90134878a119044ccbc67e6695ab811b821ea0cb
yusuf-a-s <itsderpderpson@gmail.com>
2024-07-08 23:59:37 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,13 +33,17 @@ st.markdown("""
     background-color: #90ee90; /* Light green background */
     color: black; /* Black font color */
     margin-right: 5px; /* Spacing between CALL and PUT */
+    margin-top: 5px
+    margin-bottom: 5px
     border-radius: 10px; /* Rounded corners */
 }
 
 .metric-put {
     background-color: #ffcccb; /* Light red background */
     color: black; /* Black font color */
-    margin-right: 5px; /* Spacing between CALL and PUT */
+    margin-left: 5px; /* Spacing between CALL and PUT */
+    margin-top: 5px
+    margin-bottom: 5px
     border-radius: 10px; /* Rounded corners */
 }
 
